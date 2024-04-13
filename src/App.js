6a44b7f0b9459558ea0c9ad32568bbcfd355b22c
VasyaTarn <126779570+VasyaTarn@users.noreply.github.com>
2024-04-13 13:18:35 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import News from './less1/News'
+import Features from './less1/Features'
+
+function App() {
+  return (
+    <>
+      <p>Less1</p>
+      <News />
+      <Features />
+    </>
+  )
+}
+
+export default App
+
---
+++ src/less1/Features.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Features() {
+    return (
+        <section>
+            <h1>Features</h1>
+            <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem </p>
+            <img src="/Images/img1.png" alt="" />
+        </section>
+    )
+}
+
+export default Features
